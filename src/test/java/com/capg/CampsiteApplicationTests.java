1353650f9811d61c7cab96ff435c13dc68280ea2
Bhagyalakshmi <bheemaraju.bhagyalakshmi@capgemini.com>
2023-02-26 15:09:37 +0530

    Adding project

---
+++ src/test/java/com/capg/CampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+//
+}
